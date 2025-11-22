943cd75e5e3dc62bba2f509e5a7309ada170f168
JulioGuedes01 <guedesjulio39@gmail.com>
2025-11-22 03:51:16 -0300

    First Class Position

---
+++ src/Application/Main.java
@@ -0,0 +1,11 @@
+package Application;
+
+import BoardGaming.Position;
+
+public class Main {
+    public static void main(String[] args) {
+        Position pos = new Position(4, 5);
+        System.out.print(pos);
+
+    }
+}

efac5e43c7b1dcd2fe2a25c7de48ac451d44372a
JulioGuedes01 <guedesjulio39@gmail.com>
2025-11-26 00:09:00 -0300

    BoardExceptions and defensive programming

--- src/BoardGaming/Board.java
+++ src/BoardGaming/Board.java
@@ -6,6 +6,9 @@ public class Board {
     private int coluna;
 
     public Board(int linha, int coluna) {
+        if (linha < 1 || coluna < 1) {
+            throw new BoardException("Error Creating Board");
+        }
         this.linha = linha;
         this.coluna = coluna;
         pieces = new Piece[linha][coluna];
@@ -15,25 +18,40 @@ public class Board {
         return linha;
     }
 
-    public void setLinha(int linha) {
-        this.linha = linha;
-    }
-
     public int getColuna() {
         return coluna;
     }
 
-    public void setColuna(int coluna) {
-        this.coluna = coluna;
-    }
     public Piece piece(int linha, int coluna) {
+        if (!positionExists(linha, coluna)) {
+            throw new BoardException("Position Not Found");
+        }
         return pieces[linha][coluna];
     }
     public Piece piece(Position pos) {
         return pieces[pos.getLinha()][pos.getColuna()];
     }
     public void placePiece(Piece piece, Position pos) {
+        if (thereIsAPiece(pos)){
+            throw new BoardException("There is already a Piece on Position" + pos);
+        }
         pieces[pos.getLinha()][pos.getColuna()] = piece;
         piece.pos = pos;
+    }
+
+    private boolean positionExists(int linha, int coluna) {
+       return linha >= 0 && linha < getLinha() &&
+                coluna >= 0 && coluna < getColuna();
+
+    }
+
+    public boolean positionExists(Position pos) {
+        return positionExists(pos.getLinha(), pos.getColuna());
+    }
+    public boolean thereIsAPiece(Position pos) {
+        if (!positionExists(linha, coluna)) {
+            throw new BoardException("Position Not Found");
+        }
+        return piece(pos) != null;
     }
 }
---
+++ src/BoardGaming/BoardException.java
@@ -0,0 +1,9 @@
+package BoardGaming;
+
+public class BoardException extends RuntimeException{
+    public static final long serialVersionUID=1L;
+
+    public BoardException(String message){
+        super(message);
+    }
+}
